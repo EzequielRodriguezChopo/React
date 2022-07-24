de191a96b42f8fe7ab5869bd1f4ad7220f112510
Ezequiel Rodriguez <ezequielrodriguezchopo@gmail.com>
2022-07-24 17:43:31 -0300

    Desafio: Menu E-Commerce

---
+++ panaderiaRodriguezEzequiel/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import medialuna from "../images/medialuna.jpg"
+const imagen='/medialunas.jpg'
+const NavBar = () => {
+  return (
+   
+    <div class="estiloNavAlinear">
+      <img src={medialuna} alt="" width="40" height="30" class="d-inline-block "/>
+      <button class="btn btnPersonalizado" type="submit"> Inicio </button>
+      <button class="btn btnPersonalizado" >Productos</button>
+      <button class="btn btnPersonalizado" >Nosotros</button>
+      <button class="btn btnPersonalizado" >Contacto</button>
+      <button class="btn btnPersonalizado" type="submit"> Login </button>
+      <img src={medialuna} alt="" width="40" height="30" class="d-inline-block "/>
+    </div>
+  );
+};
+export default NavBar;
